22dd6610f1d78580c6e65562c70515104f002f8e
Yusuf <yumustyologt@example.com>
2023-01-13 18:34:30 +0100

    updates

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express, { response } from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import {Configuration,OpenAIApi} from 'openai'
+
+const app = express()
+
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  
+
+  const openai = new OpenAIApi(configuration);
+
+
+  app.post('/', async(req,res)=>{
+    try {
+       const question = req.body.question;
+       const resp = await  openai.createCompletion({
+        model: "text-davinci-003",
+        prompt:`${question}`,
+        // temperature: 0.7,
+        temperature: 0,
+        max_tokens: 3000,
+        // max_tokens: 4000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+      });
+      console.log("resp ",resp.data.choices[0].text);
+      
+      res.status(200).send({
+        yung:resp.data.choices[0].text
+      })
+
+    } catch (error) {
+        console.log("error ",error.message);
+        res.status(500).send({error})
+    }
+  })
+
+  app.listen(5000,()=>console.log('listening on port 5000'))
---
+++ frontend/src/App.jsx
@@ -0,0 +1,151 @@
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+import send from "./assets/send.svg";
+import botIcon from "./assets/bot.svg";
+import userIcon from "./assets/user.svg";
+import { Configuration, OpenAIApi } from "openai";
+
+function App() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [uniqueId, setUniqueId] = useState("");
+
+  const form = useRef(null);
+  const chatContainer = useRef(null);
+
+  let loadInterval;
+
+  function loader(element) {
+    element.textContent = "";
+    loadInterval = setInterval(() => {
+      element.textContent += ".";
+      if (element.textContent === "....") {
+        element.textContent = "";
+      }
+    }, 300);
+  }
+
+  function typeText(element, text) {
+    let index = 0;
+    let interval = setInterval(() => {
+      if (index < text.length) {
+        element.innerHTML += text.charAt(index);
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 20);
+  }
+
+  function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalNumber = randomNumber.toString(16);
+    return `id-${timestamp}-${hexadecimalNumber}`;
+  }
+
+  const chatSection = (isYung, value, uniqueId) => {
+    return `<div class="wrapper ${isYung && "ai"}">
+            <div class="chat">
+              <div class="profile">
+                <img src="${isYung ? botIcon : userIcon}" 
+                alt="${isYung ? "botIcon" : "userIcon"}"
+                />
+              </div>
+              <div class="message" id="${uniqueId}">
+                ${value}
+              </div>
+            </div>
+          </div> 
+        `;
+  };
+
+ 
+  const getAnswers = async () => {
+   
+    if (question.length === 0) return;
+    
+    const resp = fetch("http://localhost:5000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        question,
+      }),
+    })
+      .then((data) => data.json())
+      .then((response) => {
+        clearInterval(loadInterval);
+        setAnswer(response);
+        setQuestion("");
+      })
+      .catch((error) => {
+        console.log("theres an error ", error);
+        messageDiv.current.innerHTML =
+          "Something went nuts 🥜🥜. Please try again.";
+      });
+  };
+
+  useEffect(() => {
+    const messageDiv = document.getElementById(uniqueId);
+
+    if (answer) {
+      clearInterval(loadInterval);
+      const parsedData = answer.yung.trim();
+      typeText(messageDiv, parsedData);
+      setAnswer('')
+      return;
+    }
+
+    if (!uniqueId) return;
+    loader(messageDiv);
+    getAnswers();
+    messageDiv.innerHTML = "";
+  }, [uniqueId, answer]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    //user chat section
+    chatContainer.current.innerHTML += chatSection(false, question);
+    // setQuestion("");
+
+    // bot's chatStripe
+    const uniqueId = generateUniqueId();
+    setUniqueId(uniqueId);
+    console.log(uniqueId);
+    chatContainer.current.innerHTML += chatSection(true, " ", uniqueId);
+    chatContainer.current.scrollTop = chatContainer.current.scrollHeight;
+  };
+
+  // form.current.addEventListener('submit',handleSubmit);
+  // form.current.addEventListener('keyup',e=>{
+  //   if(e.keyCode === 13){
+  //     handleSubmit(e)
+  //   }
+  // });
+
+  return (
+    <div id="app">
+      <div id="chat_container" ref={chatContainer}></div>
+      <form>
+        <textarea
+          name="prompt"
+          rows="1"
+          cols="1"
+          placeholder="Ask Yung..."
+          ref={form}
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        ></textarea>
+        <button type="button" onClick={handleSubmit}>
+          <img src={send} />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
